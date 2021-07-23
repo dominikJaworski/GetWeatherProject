31825bcf578ee2eeb1b21a1905252876b26ec70d
Dominik Jaworski <dominik777@outlook.com>
2021-07-23 00:09:08 -0400

    First commit

---
+++ src/Components/BiweeklyForecast.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import 'tachyons';
+
+const BiweeklyForecast = (props) => {
+    return(
+        <div>{props.location} biweeklyforecast alright</div>
+    )
+
+}
+
+export default BiweeklyForecast;
