c3613136b604c7528f375a27b40669a2c05ad7d2
Dominik Jaworski <dominik777@outlook.com>
2021-08-07 21:18:51 -0400

    Aug 7

--- src/App.js
+++ src/App.js
@@ -13,8 +13,9 @@ const App = (props) => {
   const [location, setLocation] = useState("Detroit");
   const [locationLat, setLat] = useState("");
   const [locationLong, setLong] = useState("");
-  const [forecastType, setForecastType] = useState(3);
+  const [forecastType, setForecastType] = useState(0);
   const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
 
   //set state
   useEffect(() => {
@@ -22,12 +23,14 @@ const App = (props) => {
       try {
         const result = await fetch(`http://www.mapquestapi.com/geocoding/v1/address?key=${APIkey}&location=${location}`);
 
+        setIsLoaded(true);
         const JSONresult = await result.json()
         setLat(JSONresult.results[0].locations[0].latLng.lat);
         setLong(JSONresult.results[0].locations[0].latLng.lng);
         console.log("At mount: Latitude = ", locationLat, ", Longitude = ", locationLong);
       }
-      catch(error){
+      catch (error) {
+        setIsLoaded(true);
         setError(error);
       }
     }
@@ -38,21 +41,21 @@ const App = (props) => {
   const onSearchFieldChange = (event) => {
     setSearchField(event.target.value);
   }
-  
+
   const onSearchFieldSubmit = (event) => {
     setLocation(searchField);
     //alert('new location will be :' + this.state.searchField);
     event.preventDefault();
   }
-  
+
   const onCurrentForecastClick = () => {
     setForecastType(0);
   }
-  
+
   const onHourlyForecast = () => {
     setForecastType(1);
   }
-  
+
   const onBiweeklyForecast = () => {
     setForecastType(2);
   }
@@ -75,39 +78,43 @@ const App = (props) => {
       weatherType = <CurrentForecast latitude={locationLat} longitude={locationLong}></CurrentForecast>
   }
 
-  return (
-    <div className='tc'>
-      <h1 style={{color: 'white'}}>Weather forecast</h1>
-      {/* <SearchLocation searchField={this.onSearchFieldChange}>
+  if (!isLoaded) {
+    return (<div>Loading...</div>)
+  } else {
+    return (
+      <div className='tc'>
+        <h1 style={{ color: 'white' }}>Weather forecast</h1>
+        {/* <SearchLocation searchField={this.onSearchFieldChange}>
       </SearchLocation> */}
 
-      <form className='pa2' onSubmit={onSearchFieldSubmit}>
-        <input
-          className='pa3 ba b--green bg-lightest-blue'
-          type='search'
-          placeholder='Search Locations'
-          onChange={onSearchFieldChange}
-        />
-        <input type="submit" value="Submit" className='ba tc pa2 white bg-blue hover-bg-light-blue br2 shadow-2'></input>
-      </form>
-      <p>{searchField}</p>
-      <p>{locationLat}, {locationLong}</p>
-      <button
-        className='current-forecast-button ba tc pa2 white bg-blue hover-bg-light-blue br2 shadow-2'
-        onClick={() => onCurrentForecastClick(searchField)}>Current Forecast</button>
+        <form className='pa2' onSubmit={onSearchFieldSubmit}>
+          <input
+            className='pa3 ba b--green bg-lightest-blue'
+            type='search'
+            placeholder='Search Locations'
+            onChange={onSearchFieldChange}
+          />
+          <input type="submit" value="Submit" className='ba tc pa2 white bg-blue hover-bg-light-blue br2 shadow-2'></input>
+        </form>
+        <p>{searchField}</p>
+        <p>{locationLat}, {locationLong}</p>
+        <button
+          className='current-forecast-button ba tc pa2 white bg-blue hover-bg-light-blue br2 shadow-2'
+          onClick={() => onCurrentForecastClick(searchField)}>Current Forecast</button>
 
-      <button
-        className='current-forecast-button ba tc pa2 white bg-green hover-bg-light-green br2 shadow-2'
-        onClick={() => onHourlyForecast()}>Hourly Forecast</button>
+        <button
+          className='current-forecast-button ba tc pa2 white bg-green hover-bg-light-green br2 shadow-2'
+          onClick={() => onHourlyForecast()}>Hourly Forecast</button>
 
-      <button
-        className='current-forecast-button ba tc pa2 white bg-purple hover-bg-light-purple br2 shadow-2'
-        onClick={() => onBiweeklyForecast()}>Biweekly Forecast</button>
+        <button
+          className='current-forecast-button ba tc pa2 white bg-purple hover-bg-light-purple br2 shadow-2'
+          onClick={() => onBiweeklyForecast()}>Biweekly Forecast</button>
 
-      {weatherType}
+        {weatherType}
 
-    </div>
-  );
+      </div>
+    );
+  }
 
 }
 
